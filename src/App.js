e5ea36b62285bf3bff22783a48beecff210c9e9c
graygabrielle <gray.atkinson.adams@gmail.com>
2019-07-13 21:20:10 -0700

    set up alerts and clear alerts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import axios from "axios";
 import "./App.css";
@@ -9,7 +10,8 @@ import "./App.css";
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   // async componentDidMount() {
@@ -38,17 +40,32 @@ class App extends Component {
     this.setState({ users: [], loading: false });
   };
 
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
+  clearAlert = () => {
+    this.setState({ alert: null });
+  };
+
   render() {
+    const { users, loading } = this.state;
+
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
-            showClear={this.state.users.length > 0 ? true : false}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+            clearAlert={this.clearAlert}
           />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
